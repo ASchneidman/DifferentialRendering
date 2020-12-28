bd6e2c016ad20d2f17010028f00267b7d387300c
ASchneidman <alexmarkschneidman@gmail.com>
2020-12-27 20:05:47 -0600

    fixed many bugs

---
+++ vilbert_utils.py
@@ -0,0 +1,347 @@
+import sys
+import os
+import torch
+import yaml
+
+sys.path.append('vilbert-multi-task/')
+
+from easydict import EasyDict as edict
+from pytorch_transformers.tokenization_bert import BertTokenizer
+from vilbert.datasets import ConceptCapLoaderTrain, ConceptCapLoaderVal
+from vilbert.vilbert import VILBertForVLTasks, BertConfig, BertForMultiModalPreTraining
+from vilbert.task_utils import LoadDatasetEval
+
+import numpy as np
+import matplotlib.pyplot as plt
+import PIL
+
+from maskrcnn_benchmark.config import cfg
+from maskrcnn_benchmark.layers import nms
+from maskrcnn_benchmark.modeling.detector import build_detection_model
+from maskrcnn_benchmark.structures.image_list import to_image_list
+from maskrcnn_benchmark.utils.model_serialization import load_state_dict
+from PIL import Image
+import cv2
+import argparse
+import glob
+from types import SimpleNamespace
+import pdb
+
+import torchvision
+
+
+class FeatureExtractor:
+    MAX_SIZE = 1333
+    MIN_SIZE = 800
+
+    def __init__(self):
+        self.args = self.get_parser()
+        self.detection_model = self._build_detection_model()
+
+    def get_parser(self):
+        parser = SimpleNamespace(model_file= 'vilbert-multi-task/data/detectron_model.pth',
+                                config_file='vilbert-multi-task/data/detectron_config.yaml',
+                                batch_size=1,
+                                num_features=100,
+                                feature_name="fc6",
+                                confidence_threshold=0,
+                                background=False,
+                                partition=0)
+        return parser
+    
+    def _build_detection_model(self):
+        cfg.merge_from_file(self.args.config_file)
+        cfg.freeze()
+
+        model = build_detection_model(cfg)
+        checkpoint = torch.load(self.args.model_file, map_location=torch.device("cpu"))
+
+        load_state_dict(model, checkpoint.pop("model"))
+
+        model.to("cuda")
+        model.eval()
+        return model
+
+    def _image_transform_tensor(self, im):
+        # IndexError: too many indices for array, grayscale images
+        print(im.shape)
+        if len(im.shape) < 3:
+            im = torch.repeat(im[:, :, np.newaxis], 3, axis=2)
+        im = torch.flip(im, [2])
+        im -= torch.tensor([102.9801, 115.9465, 122.7717]).to("cuda")
+        im_shape = torch.tensor(im.shape)
+        im_height = im_shape[0]
+        im_width = im_shape[1]
+        im_size_min = torch.min(im_shape[0:2])
+        im_size_max = torch.max(im_shape[0:2])
+
+        # Scale based on minimum size
+        im_scale = self.MIN_SIZE / im_size_min
+
+        # Prevent the biggest axis from being more than max_size
+        # If bigger, scale it down
+        if torch.round(im_scale * im_size_max) > self.MAX_SIZE:
+            im_scale = self.MAX_SIZE / im_size_max
+
+        # takes size as (h, w)
+        resizer = torchvision.transforms.Resize((int(im_scale * im.shape[0]), int(im_scale * im.shape[1])))
+        # expects (c, h, w)
+        img = resizer(im.permute(2, 0, 1))
+        #im = cv2.resize(
+        #    im, None, None, fx=im_scale, fy=im_scale, interpolation=cv2.INTER_LINEAR
+        #)
+        #img = im.permute(2, 0, 1)
+
+        im_info = {"width": im_width, "height": im_height}
+
+        return img, im_scale, im_info
+
+    def _image_transform(self, path):
+        img = Image.open(path)
+        im = np.array(img).astype(np.float32)
+        # IndexError: too many indices for array, grayscale images
+        if len(im.shape) < 3:
+            im = np.repeat(im[:, :, np.newaxis], 3, axis=2)
+        im = im[:, :, ::-1]
+        im -= np.array([102.9801, 115.9465, 122.7717])
+        im_shape = im.shape
+        im_height = im_shape[0]
+        im_width = im_shape[1]
+        im_size_min = np.min(im_shape[0:2])
+        im_size_max = np.max(im_shape[0:2])
+
+        # Scale based on minimum size
+        im_scale = self.MIN_SIZE / im_size_min
+
+        # Prevent the biggest axis from being more than max_size
+        # If bigger, scale it down
+        if np.round(im_scale * im_size_max) > self.MAX_SIZE:
+            im_scale = self.MAX_SIZE / im_size_max
+
+        im = cv2.resize(
+            im, None, None, fx=im_scale, fy=im_scale, interpolation=cv2.INTER_LINEAR
+        )
+        img = torch.from_numpy(im).permute(2, 0, 1)
+
+        im_info = {"width": im_width, "height": im_height}
+
+        return img, im_scale, im_info
+
+    def _process_feature_extraction(
+        self, output, im_scales, im_infos, feature_name="fc6", conf_thresh=0
+    ):
+        batch_size = len(output[0]["proposals"])
+        n_boxes_per_image = [len(boxes) for boxes in output[0]["proposals"]]
+        score_list = output[0]["scores"].split(n_boxes_per_image)
+        score_list = [torch.nn.functional.softmax(x, -1) for x in score_list]
+        feats = output[0][feature_name].split(n_boxes_per_image)
+        cur_device = score_list[0].device
+
+        feat_list = []
+        info_list = []
+
+        for i in range(batch_size):
+            dets = output[0]["proposals"][i].bbox / im_scales[i]
+            scores = score_list[i]
+            max_conf = torch.zeros((scores.shape[0])).to(cur_device)
+            conf_thresh_tensor = torch.full_like(max_conf, conf_thresh)
+            start_index = 1
+            # Column 0 of the scores matrix is for the background class
+            if self.args.background:
+                start_index = 0
+            for cls_ind in range(start_index, scores.shape[1]):
+                cls_scores = scores[:, cls_ind]
+                keep = nms(dets, cls_scores, 0.5)
+                max_conf[keep] = torch.where(
+                    # Better than max one till now and minimally greater than conf_thresh
+                    (cls_scores[keep] > max_conf[keep])
+                    & (cls_scores[keep] > conf_thresh_tensor[keep]),
+                    cls_scores[keep],
+                    max_conf[keep],
+                )
+
+            sorted_scores, sorted_indices = torch.sort(max_conf, descending=True)
+            num_boxes = (sorted_scores[: self.args.num_features] != 0).sum()
+            keep_boxes = sorted_indices[: self.args.num_features]
+            feat_list.append(feats[i][keep_boxes])
+            bbox = output[0]["proposals"][i][keep_boxes].bbox / im_scales[i]
+            # Predict the class label using the scores
+            objects = torch.argmax(scores[keep_boxes][start_index:], dim=1)
+            cls_prob = torch.max(scores[keep_boxes][start_index:], dim=1)
+
+            info_list.append(
+                {
+                    "bbox": bbox.cpu().numpy(),
+                    "num_boxes": num_boxes.item(),
+                    "objects": objects.cpu().numpy(),
+                    "image_width": im_infos[i]["width"],
+                    "image_height": im_infos[i]["height"],
+                    "cls_prob": scores[keep_boxes].cpu().numpy(),
+                }
+            )
+
+        return feat_list, info_list
+
+    def get_detectron_features(self, image_paths):
+        img_tensor, im_scales, im_infos = [], [], []
+
+        for image_path in image_paths:
+            if isinstance(image_path, torch.Tensor):
+                im, im_scale, im_info = self._image_transform_tensor(image_path)
+            else:
+                im, im_scale, im_info = self._image_transform(image_path)
+            img_tensor.append(im)
+            im_scales.append(im_scale)
+            im_infos.append(im_info)
+
+        # Image dimensions should be divisible by 32, to allow convolutions
+        # in detector to work
+        current_img_list = to_image_list(img_tensor, size_divisible=32)
+        current_img_list = current_img_list.to("cuda")
+
+        #with torch.no_grad():
+        output = self.detection_model(current_img_list)
+
+        feat_list = self._process_feature_extraction(
+            output,
+            im_scales,
+            im_infos,
+            self.args.feature_name,
+            self.args.confidence_threshold,
+        )
+
+        return feat_list
+
+    def _chunks(self, array, chunk_size):
+        for i in range(0, len(array), chunk_size):
+            yield array[i : i + chunk_size]
+
+    def _save_feature(self, file_name, feature, info):
+        file_base_name = os.path.basename(file_name)
+        file_base_name = file_base_name.split(".")[0]
+        info["image_id"] = file_base_name
+        info["features"] = feature.cpu().numpy()
+        file_base_name = file_base_name + ".npy"
+
+        np.save(os.path.join(self.args.output_folder, file_base_name), info)
+
+    def extract_features(self, image_path):
+
+        features, infos = self.get_detectron_features([image_path])
+
+        return features, infos
+
+
+def tokenize_batch(batch, tokenizer):
+    return [tokenizer.convert_tokens_to_ids(sent) for sent in batch]
+
+def untokenize_batch(batch, tokenizer):
+    return [tokenizer.convert_ids_to_tokens(sent) for sent in batch]
+
+def detokenize(sent, tokenizer):
+    """ Roughly detokenizes (mainly undoes wordpiece) """
+    new_sent = []
+    for i, tok in enumerate(sent):
+        if tok.startswith("##"):
+            new_sent[len(new_sent) - 1] = new_sent[len(new_sent) - 1] + tok[2:]
+        else:
+            new_sent.append(tok)
+    return new_sent
+
+# write arbitary string for given sentense. 
+import _pickle as cPickle
+
+def prediction(
+    model, 
+    question, 
+    features, 
+    spatials, 
+    segment_ids, 
+    input_mask, 
+    image_mask, 
+    co_attention_mask, 
+    task_tokens
+):
+
+    (
+        vil_prediction, 
+        vil_prediction_gqa, 
+        vil_logit, 
+        vil_binary_prediction, 
+        vil_tri_prediction, 
+        vision_prediction, 
+        vision_logit, 
+        linguisic_prediction, 
+        linguisic_logit, 
+        attn_data_list
+    ) = model(
+            question, 
+            features, 
+            spatials, 
+            segment_ids, 
+            input_mask, 
+            image_mask, 
+            co_attention_mask, 
+            task_tokens, 
+            output_all_attention_masks=True
+    )
+    
+    return vil_prediction
+
+def custom_prediction(model, query, task, features, infos, tokenizer):
+    tokens = tokenizer.encode(query)
+    tokens = tokenizer.add_special_tokens_single_sentence(tokens)
+
+    segment_ids = [0] * len(tokens)
+    input_mask = [1] * len(tokens)
+
+    max_length = 37
+    if len(tokens) < max_length:
+        # Note here we pad in front of the sentence
+        padding = [0] * (max_length - len(tokens))
+        tokens = tokens + padding
+        input_mask += padding
+        segment_ids += padding
+
+    text = torch.from_numpy(np.array(tokens)).cuda().unsqueeze(0)
+    input_mask = torch.from_numpy(np.array(input_mask)).cuda().unsqueeze(0)
+    segment_ids = torch.from_numpy(np.array(segment_ids)).cuda().unsqueeze(0)
+    task = torch.from_numpy(np.array(task)).cuda().unsqueeze(0)
+
+    num_image = len(infos)
+
+    feature_list = []
+    image_location_list = []
+    image_mask_list = []
+    for i in range(num_image):
+        image_w = infos[i]['image_width']
+        image_h = infos[i]['image_height']
+        feature = features[i]
+        num_boxes = feature.shape[0]
+
+        g_feat = torch.sum(feature, dim=0) / num_boxes
+        num_boxes = num_boxes + 1
+        feature = torch.cat([g_feat.view(1,-1), feature], dim=0)
+        boxes = infos[i]['bbox']
+        image_location = np.zeros((boxes.shape[0], 5), dtype=np.float32)
+        image_location[:,:4] = boxes
+        image_location[:,4] = (image_location[:,3] - image_location[:,1]) * (image_location[:,2] - image_location[:,0]) / (float(image_w) * float(image_h))
+        image_location[:,0] = image_location[:,0] / float(image_w)
+        image_location[:,1] = image_location[:,1] / float(image_h)
+        image_location[:,2] = image_location[:,2] / float(image_w)
+        image_location[:,3] = image_location[:,3] / float(image_h)
+        g_location = np.array([0,0,1,1,1])
+        image_location = np.concatenate([np.expand_dims(g_location, axis=0), image_location], axis=0)
+        image_mask = [1] * (int(num_boxes))
+
+        feature_list.append(feature)
+        image_location_list.append(torch.tensor(image_location))
+        image_mask_list.append(torch.tensor(image_mask))
+
+    features = torch.stack(feature_list, dim=0).float().cuda()
+    spatials = torch.stack(image_location_list, dim=0).float().cuda()
+    image_mask = torch.stack(image_mask_list, dim=0).byte().cuda()
+    co_attention_mask = torch.zeros((num_image, num_boxes, max_length)).cuda()
+
+    return prediction(text, features, spatials, segment_ids, input_mask, image_mask, co_attention_mask, task)
+
